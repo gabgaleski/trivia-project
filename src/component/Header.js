1bd480a66602b47a32f95a63709a32c22c1d0ed0
Leandro Motta <leandromotta02@hotmail.com>
2023-04-19 17:42:41 -0300

    req5

---
+++ src/component/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  imageGravatar = () => {
+    const { email } = this.props;
+    const md = md5(email).toString();
+    const link = (`https://www.gravatar.com/avatar/${md}`);
+    return link;
+  };
+
+  render() {
+    const { name } = this.props;
+    const image = this.imageGravatar();
+    return (
+      <header>
+        <img src={ image } alt="..." data-testid="header-profile-picture" />
+        <h4 data-testid="header-player-name">{name}</h4>
+        <h3 data-testid="header-score">0</h3>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  name: state.reducer.login.name,
+  email: state.reducer.login.email,
+});
+
+export default connect(mapStateToProps)(Header);
