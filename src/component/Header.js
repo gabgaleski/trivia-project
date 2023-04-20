f8332adc72abd5df5af081964ab041b98bba5065
Leandro Motta <leandromotta02@hotmail.com>
2023-04-20 19:09:17 -0300

    req12

--- src/component/Header.js
+++ src/component/Header.js
@@ -12,13 +12,13 @@ class Header extends Component {
   };
 
   render() {
-    const { name } = this.props;
+    const { name, score } = this.props;
     const image = this.imageGravatar();
     return (
       <header>
         <img src={ image } alt="..." data-testid="header-profile-picture" />
         <h4 data-testid="header-player-name">{name}</h4>
-        <h3 data-testid="header-score">0</h3>
+        <h3 data-testid="header-score">{score}</h3>
       </header>
     );
   }
@@ -27,11 +27,13 @@ class Header extends Component {
 Header.propTypes = {
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   name: state.reducer.login.name,
   email: state.reducer.login.email,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Header);
