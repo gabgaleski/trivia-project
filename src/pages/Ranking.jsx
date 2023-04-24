2c0c44435181d21ba053da5420ca8fc828037936
Leandro Motta <leandromotta02@hotmail.com>
2023-04-24 15:57:05 -0300

    req18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <h1 data-testid="ranking-title"> Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ () => { history.push('/'); } }
+        >
+          Tela Inicial
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
