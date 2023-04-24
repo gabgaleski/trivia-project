1f7428c4d00f10f40a3b6c78440ef9b4fe5850fd
Leandro Motta <leandromotta02@hotmail.com>
2023-04-24 15:50:25 -0300

    req16

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -22,6 +22,12 @@ class Feedback extends Component {
         >
           Play Again
         </button>
+        <button
+          data-testid="btn-ranking"
+          onClick={ () => { history.push('/ranking'); } }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
